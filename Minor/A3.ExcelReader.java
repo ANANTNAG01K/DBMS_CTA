78aefa1df8217f8bb51d345696cb3a3cf9732c57
ANANTNAG01K <163745622+ANANTNAG01K@users.noreply.github.com>
2024-10-21 22:19:17 +0530

    Create A3.ExcelReader.java

---
+++ Minor/A3.ExcelReader.java
@@ -0,0 +1,43 @@
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ExcelReader {
+    public static void main(String[] args) {
+        String excelFilePath = "CopilotAnswers-20240318-195424.xlsx"; // Change this to your file path
+
+        try (FileInputStream fis = new FileInputStream(new File(excelFilePath));
+             Workbook workbook = new XSSFWorkbook(fis)) {
+
+            Sheet sheet = workbook.getSheetAt(0); // Access the first sheet
+
+            for (Row row : sheet) {
+                for (Cell cell : row) {
+                    switch (cell.getCellType()) {
+                        case STRING:
+                            System.out.print(cell.getStringCellValue() + "\t");
+                            break;
+                        case NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "\t");
+                            break;
+                        case BOOLEAN:
+                            System.out.print(cell.getBooleanCellValue() + "\t");
+                            break;
+                        case FORMULA:
+                            System.out.print(cell.getCellFormula() + "\t");
+                            break;
+                        default:
+                            System.out.print("Unknown Cell Type\t");
+                    }
+                }
+                System.out.println(); // New line after each row
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
